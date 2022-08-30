e6bcdf9e02cabca0ee9210c6ad430d4609a7b91d
1812 <francogadea1812@gmail.com>
2022-08-30 15:25:33 -0300

    ahora si

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
-      
+      <Title></Title>
     </div>
   );
 }
